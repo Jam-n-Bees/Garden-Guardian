81098e2612bee033cfa5606fe93f2380e6bbe607
Brendon <elderberryjamproductions@outlook.com>
2025-03-17 21:17:27 +0000

    Simple Enemy Movement - Right Side
    Only scripted for the right side, but when entering the area, the enemy is entered into the array, and when the mouse 2 button is clicked, the player zips over to it and "Defeats" it

--- player.gd
+++ player.gd
@@ -3,6 +3,7 @@ extends Node2D
 var attack_cooldown_stat := 3
 var attack_cooldown_time : float
 @onready var attack_on_cooldown := false
+var punch_zone_res := load("res://punch_zone.tres")
 
 
 func _process(delta: float) -> void:
@@ -18,10 +19,35 @@ func _process(delta: float) -> void:
 func _input(event: InputEvent) -> void:
 	if attack_on_cooldown == false:
 		if event.is_action_pressed("punch_right"):
-			self.position.x += 200
-			attack_on_cooldown = true
-			attack_cooldown_time = attack_cooldown_stat
+			if not punch_zone_res.punch_zone_right.is_empty():
+				self.position.x = punch_zone_res.punch_zone_right[0].get_parent().position.x - 100
+				await get_tree().create_timer(0.5).timeout
+				punch_zone_res.punch_zone_right[0].get_parent().queue_free()
+				punch_zone_res.punch_zone_right.remove_at(0)
+				print(str(punch_zone_res.punch_zone_right))
+				
+			else:
+				self.position.x += 200
+				attack_on_cooldown = true
+				attack_cooldown_time = attack_cooldown_stat
+			
+			
+			
 		if event.is_action_pressed("punch_left"):
 			self.position.x -= 200
 			attack_on_cooldown = true
 			attack_cooldown_time = attack_cooldown_stat
+
+
+func rs_punch_zone_detect(area: Area2D) -> void:
+	if area.is_in_group("enemy"):
+		print("Enemy detected, 3 o'clock!!!")
+		punch_zone_res.punch_zone_right.append(area)
+		print(str(punch_zone_res.punch_zone_right))
+
+
+func ls_punch_zone_detect(area: Area2D) -> void:
+	if area.is_in_group("enemy"):
+		print("Enemy detected, 9 o'clock!!!")
+		punch_zone_res.punch_zone_left.append(area)
+		print(str(punch_zone_res.punch_zone_left))
---
+++ punch_zone.gd
@@ -0,0 +1,4 @@
+extends Resource
+
+var punch_zone_right : Array
+var punch_zone_left : Array
