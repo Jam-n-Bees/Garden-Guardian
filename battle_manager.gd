b599291c9c9b5a869ff632fdfc87f6efb8d47be2
Jam-n-Bees <elderberryjamproductions@outlook.com>
2025-03-25 09:58:34 +0000

    Organised Hierarchy + Summon Goons + Class
    Added a goon class, working on the base goon, added a battle manager, and functions to simplify and automate summoning goons (as well as which side they're spawning on)

---
+++ battle_manager.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+var right_side := []
+var left_side := []
+
+var base_goon_scene = preload("res://goons/goon_scenes/base_goon.tscn")
+
+@onready var player = $Player
+
+#var rng = RandomNumberGenerator.new()
+
+func _ready() -> void:
+	summon_goon()
+	pass
+	
+func roll_100():
+	return(randf())
+	
+func summon_goon():
+	var dice_roll = roll_100()
+	var summoned_goon = base_goon_scene.instantiate()
+	summoned_goon.position.y = 750
+	if dice_roll > 0.5:
+		right_side.append(summoned_goon)
+		summoned_goon.position.x = player.position.x + 100
+	else:
+		left_side.append(summoned_goon)
+		summoned_goon.position.x = player.position.x - 100
+	add_child(summoned_goon)
+		
+		
---
+++ goons/goon_scripts/goon_class.gd
@@ -0,0 +1,11 @@
+class_name Goon
+
+extends Node2D
+
+signal attack
+signal defeated
+
+var base_speed := 20
+var base_threat := 1
+
+var slow_down := 1
