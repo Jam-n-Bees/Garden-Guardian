2dd96423f23d088bbdbc6d092e238bc08a008ad3
Jam-n-Bees <elderberryjamproductions@outlook.com>
2025-03-25 10:43:30 +0000

    Pausing and Accelerating
    new 'pause()' function. Stops the unit in its tracks, and uses a tween to accelerate back to the base speed.

--- goons/goon_scripts/goon_class.gd
+++ goons/goon_scripts/goon_class.gd
@@ -9,13 +9,23 @@ enum Incoming { RIGHT, LEFT }
 
 var goon_approach = Incoming.RIGHT
 
-var base_speed := 250
+var base_speed := 300
 var base_threat := 1
 
-var slow_down := 1
+var slow_down : float = 1
+var speed_up_delay : float = 1
 
 func _process(delta: float) -> void:
 	if goon_approach == Incoming.RIGHT:
 		self.position.x -= delta*base_speed*slow_down
 	if goon_approach == Incoming.LEFT:
 		self.position.x += delta*base_speed*slow_down
+
+func pause():
+	slow_down = 0
+	await get_tree().create_timer(1).timeout
+	var accelerate = create_tween()
+	accelerate.tween_property(self,"slow_down",1,1.5)
+	
+func _ready() -> void:
+	pause()
