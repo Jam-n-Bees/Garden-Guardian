689edf410458f2d6d1641c02be0209031cf249dd
Jam-n-Bees <elderberryjamproductions@outlook.com>
2025-03-27 13:51:53 +0000

    Updated spawning
    All enemy spawning happens at once, spaced on the size of the master array.
    
    Also, will now default to spawning to one side if the other has three or more enemies on it.

--- battle_manager.gd
+++ battle_manager.gd
@@ -10,7 +10,7 @@ var base_goon_scene = preload("res://goons/goon_scenes/base_goon.tscn")
 
 func _ready() -> void:
 	for n in range(10):
-		await get_tree().create_timer(0.5).timeout
+		# await get_tree().create_timer(0.5).timeout
 		summon_goon()
 		pass
 	
@@ -19,15 +19,19 @@ func roll_100():
 	
 func summon_goon():
 	var dice_roll = roll_100()
+	if global_variables.master_right_side.size() + 2 < global_variables.master_left_side.size():
+		dice_roll = 1
+	if global_variables.master_left_side.size() + 2 < global_variables.master_right_side.size():
+		dice_roll = 0
 	var summoned_goon = base_goon_scene.instantiate()
 	summoned_goon.position.y = 750
 	if dice_roll > 0.5:
 		global_variables.master_right_side.append(summoned_goon)
-		summoned_goon.position.x = player.position.x + 1400
+		summoned_goon.position.x = player.position.x + 1400 + 200 * global_variables.master_right_side.size() + 80 * randf()
 		summoned_goon.goon_approach = summoned_goon.Incoming.RIGHT
 	else:
 		global_variables.master_left_side.append(summoned_goon)
-		summoned_goon.position.x = player.position.x - 1400
+		summoned_goon.position.x = player.position.x - 1400 - 200 * global_variables.master_left_side.size() + 80 * randf()
 		summoned_goon.goon_approach = summoned_goon.Incoming.LEFT
 	summoned_goon.knocked_back.connect(knock_back_goons)
 	add_child(summoned_goon)
